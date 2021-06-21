9b7754c228735ac5cc0c33967021100972388756
David Palaciose <dboxing22@gmail.com>
2021-06-21 12:28:14 -0500

    commit before merge

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Journal from './client/Screens/Journal/Journal.tsx'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" /> */}
+      <Journal/>
     </View>
   );
 }
