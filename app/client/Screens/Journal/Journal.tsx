ca07df45df02307bb135ab9e555b638d284a7829
Persis <persis.randolph@gmail.com>
2021-06-23 19:23:16 -0500

    added date toggle buttons

--- app/client/Screens/Journal/Journal.tsx
+++ app/client/Screens/Journal/Journal.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import AwesomeButton from 'react-native-really-awesome-button';
 import { View, TextInput, StyleSheet, Text, ImageBackground } from 'react-native';
-import { compareAsc, format } from 'date-fns';
+import { format } from 'date-fns';
+import IconA from 'react-native-vector-icons/Ionicons';
 
 const Journal = () => {
   const bgImage = require('../../../assets/blue-gradient.png');
@@ -29,6 +30,10 @@ const Journal = () => {
           Clear Entry
           </AwesomeButton>
         </View>
+        <View style={{ flexDirection: 'row', marginLeft: 20 }}>
+          <IconA name="caret-back" size={35} color="#1D426D" />
+          <IconA name="caret-forward" size={35} color="#1D426D" />
+        </View>
         <View style={styles.textAreaContainer} >
           <Text style={styles.date}>{date}</Text>
           <TextInput
@@ -47,6 +52,16 @@ const Journal = () => {
 };
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+  },
+  button: {
+    padding: 10,
+    marginRight: 20
+  },
   container: {
     flex: 1,
     padding: 20,
@@ -57,6 +72,17 @@ const styles = StyleSheet.create({
     fontSize: 14,
     alignSelf: 'flex-end'
   },
+  header: {
+    color: '#1D426D',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginLeft: 20
+  },
+  textArea: {
+    height: '82%',
+    width: 350,
+    justifyContent: 'flex-start'
+  },
   textAreaContainer: {
     backgroundColor: '#8ebac6',
     borderRadius: 10,
@@ -64,27 +90,6 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginRight: 20,
     marginLeft: 20,
-  },
-  textArea: {
-    height: '90%',
-    width: 350,
-    justifyContent: 'flex-start'
-  },
-  header: {
-    color: '#1D426D',
-    fontSize: 22,
-    fontWeight: 'bold',
-    marginLeft: 20
-  },
-  button: {
-    padding: 10,
-    marginRight: 20
-  },
-  backgroundImage: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
   }
 });
 
