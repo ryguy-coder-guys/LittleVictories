a859e9398d61b761751d6069adf5230a66922721
Persis <persis.randolph@gmail.com>
2021-07-01 20:07:43 -0500

    fixed flat lists to show all, doesn't hide last bit now

--- app/client/Screens/Feed/FeedView.tsx
+++ app/client/Screens/Feed/FeedView.tsx
@@ -1,26 +1,44 @@
-import React from "react";
-import { FlatList, View } from "react-native";
-import { v4 as getKey } from "uuid";
-import { useFeedContext } from "../../Contexts/feedContext";
-import FeedItem from "./FeedItem";
+import React from 'react';
+import { FlatList, View, Text, StyleSheet } from 'react-native';
+import { v4 as getKey } from 'uuid';
+import { useFeedContext } from '../../Contexts/feedContext';
+import FeedItem from './FeedItem';
 
 const FeedView = () => {
   const { feed } = useFeedContext();
   return (
     <View
       style={{
-        justifyContent: "flex-start",
-        alignItems: "center",
-        marginVertical: 25,
+        justifyContent: 'flex-start',
+        marginLeft: 20,
+        marginRight: 20,
+        width: '100%'
       }}
     >
-      <FlatList
-        data={feed}
-        renderItem={({ item }) => <FeedItem {...item} />}
-        keyExtractor={() => getKey()}
-      />
+      <Text style={styles.heading}>Feed</Text>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={feed}
+          renderItem={({ item }) => <FeedItem {...item} />}
+          keyExtractor={() => getKey()}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  heading: {
+    color: '#1D426D',
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    marginTop: 25,
+    marginLeft: 20
+  },
+  listContainer: {
+    height: '88%'
+  }
+});
 
 export default FeedView;
