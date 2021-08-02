f9922f8b4d8b89586831a82fc75e830216a93426
Persis <persis.randolph@gmail.com>
2021-08-02 12:54:34 -0500

    fixed double import

--- app/client/Screens/Feed/FriendListView.tsx
+++ app/client/Screens/Feed/FriendListView.tsx
@@ -2,7 +2,6 @@ import React, { ReactElement } from 'react';
 import { FlatList } from 'react-native';
 import 'react-native-get-random-values';
 import { v4 as getKey } from 'uuid';
-import 'react-native-get-random-values';
 import SingleFriend from './SingleFriend';
 
 const FriendListView = ({ query, user, users, setUsers }): ReactElement => {
